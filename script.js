ab78ecb85bf93a7d0ea1fb265a6f413a0aae1302
sshisas <sshi.sas@gmail.com>
2023-05-24 20:07:09 -0800

    Added script.js and styles.css

---
+++ script.js
@@ -0,0 +1,20 @@
+// Smooth scrolling for navigation links
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+  
+    for (const link of navLinks) {
+      link.addEventListener('click', smoothScroll);
+    }
+  });
+  
+  function smoothScroll(event) {
+    event.preventDefault();
+  
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+  
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: 'smooth'
+    });
+  }
